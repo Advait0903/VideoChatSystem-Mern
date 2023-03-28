365727283bc762d57bdad4256cfbf62194ba7be8
advait khandare <advait.khandare@gmail.com>
2023-03-29 01:56:08 +0530

    commit-v1

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import styles from './Home.module.css'
+import {Link, useNavigate} from 'react-router-dom'
+import Card from '../../components/shared/Card/Card'
+import Button from '../../components/shared/Button/Button'
+
+
+const Home = () => {
+  const signinlinkstyle = {
+    color : '#0077ff',
+    fontWeight:'bold',
+    textDecoration : 'None',
+    marginLeft:'10px'
+  }
+
+  const navigate = useNavigate();
+  function startRegister() {
+    navigate('/register');
+ 
+  }
+
+  return (
+      <div className={styles.cardWrapper}>
+        <Card title="Welcome to VideoChat!" icon="logo">
+        <p className={styles.text}>
+            Welcome to VideoChat! Connect to your friends have chit-chat , 
+            enjoy the video with snacks.
+          </p>
+          <div>
+            <Button onClick={startRegister} text="Get your username" />
+          </div>
+
+          <div className={styles.signinwrapper}>
+            <span className={styles.invite}>Have an invite text?</span>
+            <Link style= {signinlinkstyle} to="/login">Sign In</Link>
+          </div>
+        </Card>
+      </div>
+  )
+}
+
+export default Home
