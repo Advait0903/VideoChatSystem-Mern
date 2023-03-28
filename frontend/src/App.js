e19392a66dbb69cbd16893edd9414042e2a1ff37
advait khandare <advait.khandare@gmail.com>
2023-03-29 02:00:34 +0530

    v1.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ function App() {
   <BrowserRouter>
     <Navigation />
     <Routes>
-      {/* hi */}
       <Route path="/" element={<Home/>}/>
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
